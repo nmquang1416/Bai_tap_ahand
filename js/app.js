25304525e96a0a929344b1f44e6f337b9a8a0c91
nmquang1416 <nmquang1416@gmail.com>
2023-11-22 19:48:32 +0700

    update

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var about = document.getElementById("about");
-var section_1 = document.getElementById("section-1");
-
-function scroll_about() {
-    about.addEventListener("click",function(){
-        scrollTo(section_1);
-    })
-}
-
